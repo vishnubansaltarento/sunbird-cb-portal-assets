d6b06a636f64c9c979c39950f1786ecbf47fc445
Amit Sengar <aAmitSengar@users.noreply.github.com>
2021-07-14 14:41:01 +0530

    Create env.js

---
+++ client-assets/assets/env.js
@@ -0,0 +1,9 @@
+(function (window) {
+    window["env"] = window["env"] || {};
+
+    // Environment variables
+    window["env"]["sitePath"] = 'localhost';
+    window["env"]["debug"] = '';
+    window["env"]["karmYogiPath"] = ''
+    window["env"]["cbpPath"] = ''
+})(this);

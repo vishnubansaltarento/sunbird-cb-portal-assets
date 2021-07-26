4e108075d02a719d3a4d5e611f824668ef06b8df
Amit Kumar <amit.sengar@tarento.com>
2021-07-26 17:07:25 +0530

    New changes related to Single Branch for Docker Image

---
+++ CBC/client-assets/assets/env.js
@@ -0,0 +1,9 @@
+(function (window) {
+    window["env"] = window["env"] || {};
+
+    // Environment variables
+    window["env"]["sitePath"] = '';
+    window["env"]["debug"] = '';
+    window["env"]["karmYogiPath"] = ''
+    window["env"]["cbpPath"] = ''
+})(this);

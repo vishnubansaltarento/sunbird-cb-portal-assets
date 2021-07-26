ffdae57a426d471f55c4fe660bf77e2ca0c8956c
Amit Kumar <amit.sengar@tarento.com>
2021-07-26 17:48:39 +0530

    removed dynamis files

--- CBC/client-assets/assets/env.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-    window["env"] = window["env"] || {};
-
-    // Environment variables
-    window["env"]["sitePath"] = '';
-    window["env"]["debug"] = '';
-    window["env"]["karmYogiPath"] = ''
-    window["env"]["cbpPath"] = ''
-})(this);
--- CBC/client-assets/assets/env.template.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-    window["env"] = window["env"] || {};
-
-    // Environment variables
-    window["env"]["sitePath"] = '${SITE_PATH}';
-    window["env"]["debug"] = '${DEBUG}';
-    window["env"]["karmYogiPath"] = '${KARM_YOGI_PATH}'
-    window["env"]["cbpPath"] = '${CBP_PATH}'
-})(this);

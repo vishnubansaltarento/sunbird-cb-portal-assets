098381a0f112b82c31bacf6fe0bcd414e325e5ca
Haritest <haridasksd@gmail.com>
2022-06-13 17:04:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ build_tag=$1
 name='ui-static'
 node=$2
 org=$3
+image_tag=$4
 
-docker build -f Dockerfile --label commitHash=$(git rev-parse --short HEAD) --build-arg build_tag=$image_tag -t ${org}/${name}:${build_tag} .
+
+docker build -f Dockerfile --label commitHash=$(git rev-parse --short HEAD) --build-arg build_tag=${image_tag} -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
